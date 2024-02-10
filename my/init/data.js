9c0a5f8dd12d00edcc871ef3ef3ae982c446b1f3
mohit kumar <mohitkumarjangra01@gmail.com>
2024-02-10 22:22:43 +0530

    added

---
+++ my/init/data.js
@@ -0,0 +1,112 @@
+const sampleListings = [
+  {
+    name: "Cozy Beachfront Cottage",
+  hp:100,
+  speed:200,
+    image:
+
+      "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+
+    price: 1500
+   
+  },
+  {
+    name: "Modern Loft in Downtown",
+    hp:100,
+    speed:200,
+    image:
+
+      "https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+
+    price: 1200,
+   
+  },
+  {
+    name: "Mountain Retreat",
+    hp:100,
+    speed:200,
+    image:
+
+      "https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG90ZWxzfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+
+    price: 1000,
+   
+  },
+  {
+    name: "Historic Villa in Tuscany",
+    hp:100,
+    speed:200,
+    image:
+
+      "https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG90ZWxzfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+
+    price: 2500,
+    
+  },
+  {
+    name: "Secluded Treehouse Getaway",
+    hp:100,
+  speed:200,
+    image:
+
+      "https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGhvdGVsc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+
+    price: 800,
+    
+  },
+  {
+    name: "Beachfront Paradise",
+    hp:100,
+    speed:200,
+    image:
+
+      "https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGhvdGVsc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+
+    price: 2000,
+  
+  },
+  {
+    name: "Rustic Cabin by the Lake",
+    hp:100,
+    speed:200,
+    image:
+
+      "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG1vdW50YWlufGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+
+    price: 900,
+   
+  },
+  {
+    name: "Luxury Penthouse with City Views",
+    hp:100,
+    speed:200,
+    image:
+
+      "https://images.unsplash.com/photo-1622396481328-9b1b78cdd9fd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2t5JTIwdmFjYXRpb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+
+    price: 3500,
+   
+  },
+  
+  {
+    name: "Beachfront Villa in Greece",
+    hp:100,
+    speed:200,
+    image: "https://images.unsplash.com/photo-1602343168117-bb8ffe3e2e9f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8dmlsbGF8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+
+    price: 2500,
+  
+  },
+  {
+    name: "Eco-Friendly Treehouse Retreat",
+    hp:100,
+  speed:200,
+    image: "https://images.unsplash.com/photo-1488462237308-ecaa28b729d7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c2t5JTIwdmFjYXRpb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+
+    price: 750,
+   
+  },
+
+];
+
+module.exports = { data: sampleListings };
--- my/init/index.js
+++ my/init/index.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
-const Listing = require("../models/listing.js");
+const Garage = require("../models/schema.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb://127.0.0.1:27017/account";
 
 main()
   .then(() => {
@@ -17,8 +17,8 @@ async function main() {
 }
 
 const initDB = async () => {
-  await Listing.deleteMany({});
-  await Listing.insertMany(initData.data);
+  await Garage.deleteMany({});
+  await Garage.insertMany(initData.data);
   console.log("data was initialized");
 };
 
