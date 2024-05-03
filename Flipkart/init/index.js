4b4c1716c64301e8d33f414fc08ce2ac80d01493
Mohitjangra855 <151448137+Mohitjangra855@users.noreply.github.com>
2024-05-03 18:56:11 +0530

    Update index.js

--- Flipkart/init/index.js
+++ Flipkart/init/index.js
@@ -5,11 +5,11 @@ if (process.env.NODE_ENV != "production") {
 const mongoose = require("mongoose");
 const Product = require("../models/Product");
 const Data = require("./data")
-const mongoStore = require("connect-mongo")
 
-const dbUrl = process.env.ATLASDB_URL;
 
-// const dbUrl = 'mongodb://127.0.0.1:27017/Flipkart';
+// const dbUrl = 'enter your atlas mongodb url and run to insitilized this data in your online mongodb then you run main index.js of flipkart project when you find data.....
+
+const dbUrl = 'mongodb://127.0.0.1:27017/Flipkart';
 main()
     .then(() => {
         console.log("connected to DB");
@@ -22,34 +22,10 @@ async function main() {
     await mongoose.connect(dbUrl);
 }
 
-const store = mongoStore.create({
-    mongoUrl: dbUrl,
-    crypto: {
-        secret: process.env.SECRET,
-    },
-    touchAfter: 24 * 3600,
-});
-store.on("error",()=>{
-    console.log("Session Store Error!"+ err);
-})
-
- const insertData = () =>{
-     Product.deleteMany({}).then(()=>{
-        console.log("old data deleted");
-    })
-  Product.insertMany(Data.data).then(()=>{
-        console.log("new data inserted");
-    })
-
+const insertData = async () => {
+    await Product.deleteMany({})
+    await Product.insertMany(Data.data)
+    console.log("data uploaded succesfully")
 }
-const initDB = async () => {
-    await Listing.deleteMany({});
-    // initData.data = initData.data.map((obj) => ({ ...obj, owner: "65f144c2e57cd2c28ded4f87" }))
-    initData.data = initData.data.map((obj) => ({ ...obj, owner: "66013415dae7efb0da828663" }))
-    // initData.data = initData.data.map((obj) => ({ ...obj, geometry: { type: 'Point', coordinates: [ 77.209006, 28.613895 ] },}))
-  
-    await Listing.insertMany(initData.data);
-    console.log("data was initialized");
-  };
-  
+
 insertData();
